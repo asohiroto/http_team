8c5a492537933654bd235397a2a83be7f6f2cf68
FukudaGenki <fko2647053@stu.o-hara.ac.jp>
2026-05-07 18:26:21 +0900

    player移動制限

--- DAT/Assets/Script/Player/PlayerController.cs
+++ DAT/Assets/Script/Player/PlayerController.cs
@@ -3,24 +3,61 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 0.1f;
-    
+    float dirX;
+    float dirY;
+    float xLimit = 8.5f;
+    float yLimit = 4.7f;
+    Vector3 currentPos;
+
+
     void Start()
     {
-        
+        currentPos = transform.position;
     }
 
     void FixedUpdate()
     {
-        float dirX = Input.GetAxis("Horizontal");
-        float dirY = Input.GetAxis("Vertical");
+        Move();
+    }
+
+    void Move()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        dirY = Input.GetAxis("Vertical");
 
         Vector3 moveDir = new Vector3(dirX, dirY, 0);
 
-        if(moveDir.magnitude >= 1)
+        // 画面内制限
+        if (transform.position.x >= xLimit)
+        {
+            currentPos.x = xLimit;
+        }
+
+        if (transform.position.x <= -xLimit)
+        {
+            currentPos.x = -xLimit;
+        }
+
+        if (transform.position.y >= yLimit)
+        {
+            currentPos.y = yLimit;
+        }
+
+        if (transform.position.y <= -yLimit)
+        {
+            currentPos.y = -yLimit;
+        }
+
+        // 正規化
+        if (moveDir.magnitude >= 1)
         {
             moveDir.Normalize();
         }
 
-        transform.position += moveDir * speed;
+        currentPos += moveDir * speed;
+        transform.position = currentPos;
+
+        
+
     }
 }

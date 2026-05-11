6208836b9caaa18c86e21543e8df4f310adc8a49
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-05-11 18:52:42 +0900

    up

--- DAT/Assets/Script/Coin.cs
+++ DAT/Assets/Script/Coin.cs
@@ -2,25 +2,17 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public int value = 100; // このコインの金額
-
-    // トリガー（接触）判定
+    // 接触したときに呼ばれる関数
     private void OnTriggerEnter(Collider other)
     {
-        // ぶつかった相手が「Player」タグを持っているか確認
+        // 接触した相手のタグが "Player" かどうかを確認
         if (other.CompareTag("Player"))
         {
-            // 相手（プレイヤー）のWalletスクリプトを取得
-            Wallet wallet = other.GetComponent<Wallet>();
+            // コイン獲得時の処理（ここにスコア加算などを書く）
+            Debug.Log("コインを獲得しました！");
 
-            if (wallet != null)
-            {
-                // お金を増やす処理を呼ぶ
-                wallet.AddMoney(value);
-
-                // このお金オブジェクトを消す
-                Destroy(gameObject);
-            }
+            // このオブジェクト（コイン）を削除
+            Destroy(gameObject);
         }
     }
 }

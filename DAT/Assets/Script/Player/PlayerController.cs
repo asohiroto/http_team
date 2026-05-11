813abb8fc5379e96cbec3fd8635e40d036b1cee1
FukudaGenki <fko2647053@stu.o-hara.ac.jp>
2026-05-11 18:50:39 +0900

    hi

--- DAT/Assets/Script/Player/PlayerController.cs
+++ DAT/Assets/Script/Player/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]private float speed = 0.1f;
-    [SerializeField]private float dashMulti = 5.0f;
+    [SerializeField] private float dashSpeed = 1000.0f;
     private bool onDash;
 
 
@@ -15,12 +15,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField]float yLimit = 4.7f;
     Vector3 currentPos;
     Vector3 moveDir;
+    Rigidbody2D rb;
 
 
     void Start()
     {
         currentPos = transform.position;
         onDash = false;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
@@ -31,8 +33,8 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
-        dirX = Input.GetAxis("Horizontal");
-        dirY = Input.GetAxis("Vertical");
+        dirX = Input.GetAxisRaw("Horizontal");
+        dirY = Input.GetAxisRaw("Vertical");
 
         moveDir = new Vector3(dirX, dirY, 0);
 
@@ -62,22 +64,24 @@ public class PlayerController : MonoBehaviour
         {
             moveDir.Normalize();
         }
-
-        currentPos += moveDir * speed;
-        transform.position = currentPos;
-
+        if(onDash == false)
+        {
+            currentPos += moveDir * speed;
+        }
         
-
+        transform.position = currentPos;
     }
 
     void Dash()
     {
-
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.isPressed)
         {
-            Debug.Log("space");
-            currentPos += moveDir * dashMulti * speed;
-            onDash = true;
+            if(onDash == false)
+            {
+                Debug.Log("space");
+                
+                onDash = true;
+            }
         }
     }
 }

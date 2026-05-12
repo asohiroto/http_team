3cd3adcde545f7591931b61ab81f71b5f91bb83b
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-05-12 09:25:00 +0900

    up

--- DAT/Assets/Script/UI/Coin.cs
+++ DAT/Assets/Script/UI/Coin.cs
@@ -6,7 +6,7 @@ public class Coin2D : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         // 1. まず何かが触れたら名前を出す（最優先デバッグ）
-        Debug.Log("2D接触検知！ 相手の名前: " + other.name);
+        Debug.Log("接触検知！ 相手の名前: " + other.name);
 
         // 2. タグが Player かどうか判定
         if (other.CompareTag("Player"))

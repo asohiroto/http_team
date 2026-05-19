5d854928eaf39076885158cd52f2be58758a07ac
asohiroto <fko2647059@stu.o-hara.ac.jp>
2026-05-20 00:54:47 +0900

    up

--- DAT/Assets/Script/card/HandManager.cs
+++ DAT/Assets/Script/card/HandManager.cs
@@ -2,38 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class HandManager : MonoBehaviour
 {
 
-    [SerializeField] GameObject[] CardPrefab; // 生成するカード
+    [SerializeField] GameObject[] cardPrefab; // 生成するカード
     [SerializeField] Transform[] deckCardTrans; // カードの生成場所
-    [SerializeField] int discardInd; // 破棄するカードの番地
-    [SerializeField] GameObject cursorPrefab; // カーソルのプレファブ
-
-    Vector2 cursorPos;
 
     int cardId = 0; // 生成するカードのID
     public int[] cardUseId = new int[4]; // 配列に保存されているカードIDを保存する配列
-    public int cardUse = 0; // 現在カーソルが指している手札の番号を保存
 
-    GameObject cursorInstance; // 生成したカーソルを保持する変数
+    skill_manager skill;
+
+    GameObject newCard;
 
     void Start()
     {
-        /*cursorPos.x = deckCardTrans[0].position.x + 0.8f;
-        cursorPos.y = deckCardTrans[0].position.y - 1.0f;*/
+        skill = GameObject.Find("CardManager").GetComponent<skill_manager>();
 
         for (int i = 0; i < 4; i++) // それぞれの手札の位置にランダムなカードを生成
         {
             cardId = Random.Range(0, 4);
 
-            Instantiate(CardPrefab[cardId], deckCardTrans[i]); // 初期手札の生成
+            newCard =Instantiate(cardPrefab[cardId], deckCardTrans[i]); // 初期手札の生成
+
+            ButtonListener(cardId, newCard);
 
             cardUseId[i] = cardId;
         }
-
-        //cursorInstance = Instantiate(cursorPrefab, cursorPos, Quaternion.identity); // 初期カーソルの生成(位置のみ参照)
     }
 
     // Update is called once per frame
@@ -44,44 +41,92 @@ public class HandManager : MonoBehaviour
         // 上部の数字キーによって破棄するカードを決定、カーソルを移動
         if (Keyboard.current.digit1Key.wasPressedThisFrame)
         {
-            MoveCursor(0);
-            cardUse = 0;
+            DisCard(0);
+
+            newCard = CardGenerate(nextCardId, 0);
+            cardUseId[0] = nextCardId;
+
+            ButtonListener(nextCardId, newCard);
         }
 
-        if (Keyboard.current.digit2Key.wasPressedThisFrame) 
-        { 
-            MoveCursor(1);
-            cardUse = 1;
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            DisCard(1);
+
+            newCard = CardGenerate(nextCardId, 1);
+            cardUseId[1] = nextCardId;
+
+            ButtonListener(nextCardId, newCard);
+
         }
         if (Keyboard.current.digit3Key.wasPressedThisFrame)
         {
-            MoveCursor(2);
-            cardUse = 2;
-        }
-        if(Keyboard.current.digit4Key.wasPressedThisFrame) 
-        { 
-            MoveCursor(3);
-            cardUse = 3;
-        }
+            DisCard(2);
 
+            newCard = CardGenerate(nextCardId, 2);
+            cardUseId[2] = nextCardId;
 
-        if (Keyboard.current.wKey.wasPressedThisFrame)
+            ButtonListener(nextCardId, newCard);
+
+        }
+        if (Keyboard.current.digit4Key.wasPressedThisFrame)
         {
-            Card_Change(nextCardId, discardInd);
-            cardUseId[discardInd] = nextCardId;
+            DisCard(3);
 
+            newCard = CardGenerate(nextCardId, 3);
+            cardUseId[3] = nextCardId;
+
+            ButtonListener(nextCardId, newCard);
         }
+
     }
 
-    void MoveCursor(int index)
+    GameObject CardGenerate(int ran, int chan) // 特定のカードを捨て、新たにランダムなカードを生成する
     {
-        discardInd = index;
-        cursorInstance.transform.position = (Vector2)deckCardTrans[index].position + new Vector2(0.8f, -1.0f); // カーソルの位置を選択した場所へ移動させる
+        GameObject genCard =Instantiate(cardPrefab[ran], deckCardTrans[chan]); // カードを作る処理
+
+        return genCard;
     }
 
-    void Card_Change(int ran, int chan) // 特定のカードを捨て、新たにランダムなカードを生成する
+    void DisCard(int chan)
     {
-        Destroy(deckCardTrans[chan].GetChild(0).gameObject); // カードを捨てる処理
-        Instantiate(CardPrefab[ran], deckCardTrans[chan]); // カードを作る処理
+        Destroy(deckCardTrans[chan].GetChild(0).gameObject);
+    }
+
+    void ButtonListener(int Ind, GameObject targetCard)
+    {
+
+        Button btn = targetCard.GetComponentInChildren<Button>();
+
+        // ボタンコンポーネントがついていなかった場合の安全装置
+        if (btn == null) return;
+
+        switch (Ind) 
+        {
+            case 0:
+
+                btn.onClick.AddListener(skill.Enhance);
+
+                break;
+
+            case 1:
+
+                btn.onClick.AddListener(skill.Heal);
+
+                break;
+
+            case 2:
+
+                btn.onClick.AddListener(skill.Slash);
+
+                break;
+
+            case 3:
+
+                btn.onClick.AddListener(skill.FireBall);
+
+                break;
+        }
+        
     }
 }
--- DAT/Assets/Script/card/skill_manager.cs
+++ DAT/Assets/Script/card/skill_manager.cs
@@ -1,27 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class skill_manager : MonoBehaviour
 {
-
-    heal_manager heal;
-    enhance_manager enhance;
     player_manager_kari player;
-    HandManager hand;
 
-    [SerializeField] int effectTime; // 強化の効果時間
-    [SerializeField] int heal_amount; // 回復量
-
-    int time = 0; // 時間カウンタ
-    int enhanceFlag = 0; // 攻撃力強化のフラグ
+    [SerializeField] int healAmount;
+    [SerializeField] int enhanceAmount;
+    [SerializeField] int enhanceTime;
+    int time = 0;
+    int enhanceFlag = 0;
+    
 
     void Start()
     {
-        heal = GetComponent<heal_manager>();
-        enhance = GetComponent<enhance_manager>();
-        hand = GameObject.Find("HandManager").GetComponent<HandManager>();
         player = GameObject.Find("Square").GetComponent<player_manager_kari>();
-
+        
     }
 
     // Update is called once per frame
@@ -29,68 +24,52 @@ public class skill_manager : MonoBehaviour
     {
         time++;
 
-        if (Mouse.current.rightButton.wasPressedThisFrame)
+        if(enhanceFlag == 1 && time > enhanceTime * 60)
         {
-            switch (hand.cardUseId[hand.cardUse]) // カーソル位置に応じて発動分岐
-            {
-                case 0: // 強化発動
-                    if (enhanceFlag == 0)
-                    {
-                        enhance.Enhance();
-                        time = 0;
-                        enhanceFlag = 1; // フラグ立て
-                    }
+            player.power = player.firstPower;
+            Debug.Log("Power : " + player.power);
 
-                    Debug.Log(hand.cardUseId[hand.cardUse]);
-
-                    Debug.Log("選択中　1");
-
-                    break;
-
-                case 1: // 回復発動
-                    
-                    if (player.hp <= player.maxHp - heal_amount) // 体力の減少量が回復量よりも大きければ、回復量分だけ回復
-                    {
-                        heal.Heal(heal_amount);
-
-
-                    }
-                    else // 体力の減少量が回復量よりも少なければ、体力の最大値になるように回復
-                    {
-                        heal.Heal(player.maxHp - player.hp);
-                    }
-
-                    Debug.Log("選択中　2");
-                    Debug.Log(hand.cardUseId[hand.cardUse]);
-
-                    break;
-
-                case 2:
-
-                    Debug.Log("選択中　3");
-                    Debug.Log(hand.cardUseId[hand.cardUse]);
-
-                    break;
-
-                case 3:
-
-                    Debug.Log("選択中　4");
-                    Debug.Log(hand.cardUseId[hand.cardUse]);
-
-                    break;
-
-                default:
-                    break;
-
-            }
-
+            enhanceFlag = 0;
         }
 
-        if (time > effectTime * 60 && enhanceFlag == 1) // 強化から２秒経過かつ、フラグが立っていればリセット
-        {
-            enhance.Enhance_reset();
-            enhanceFlag = 0; // フラグ下げ
 
-        }
     }
+
+
+    public void Slash()
+    {
+
+    }
+
+    public void Heal()
+    {
+        if(player.hp > player.maxHp - healAmount)
+        {
+            player.hp = player.maxHp;
+        }
+        else
+        {
+            player.hp += healAmount;
+        }
+
+        Debug.Log(player.hp);
+    }
+
+    public void Enhance()
+    {
+        player.power += enhanceAmount;
+
+        enhanceFlag = 1;
+        time = 0;
+
+        Debug.Log(player.power);
+    }
+
+    public void FireBall()
+    {
+
+    }
+
+
 }
+

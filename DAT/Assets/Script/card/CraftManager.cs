c22a944d5be36901ccb13cda9073288866eac6f1
asohiroto <fko2647059@stu.o-hara.ac.jp>
2026-05-20 11:47:55 +0900

    up

--- DAT/Assets/Script/card/CraftManager.cs
+++ DAT/Assets/Script/card/CraftManager.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 
 public class CraftManager : MonoBehaviour
 {
-    [SerializeField] private List<CraftRecipe> recipeList;
+    [SerializeField] private List<CraftRecipe> recipeList; // レシピのリストUnityの画面上にを作成
 
-    private Dictionary<int, int> recipeDictionary = new Dictionary<int, int>();
+    private Dictionary<int, int> recipeDictionary = new Dictionary<int, int>(); // レシピを収めるレシピ本を作成
 
     void Start()
     {
-        foreach (var recipe in recipeList)
+        foreach (var recipe in recipeList) // レシピ本の索引を作成
         {
             int key = CreateRecipeKey(recipe.materialA, recipe.materialB);
 
             if (!recipeDictionary.ContainsKey(key))
             {
-                recipeDictionary.Add(key, recipe.result);
+                recipeDictionary.Add(key, recipe.result); // 索引を登録
             }
         }
     }
 
-    private int CreateRecipeKey(int item1, int item2)
+    private int CreateRecipeKey(int item1, int item2) // レシピの索引用の鍵を作成
     {
         if (item1 < item2)
         {
@@ -32,7 +32,7 @@ public class CraftManager : MonoBehaviour
         }
     }
 
-    public int CombineItems(int input1, int input2)
+    public int CombineItems(int input1, int input2) // 鍵とレシピをもとに合成、カードの配列内の位置を示す数値を返す
     {
         int searchKey = CreateRecipeKey(input1, input2);
 

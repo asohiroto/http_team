81357e08d4325163e3bbdbe139dad63855f9d980
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-05-27 14:53:32 +0900

    不要スクリプト削除

--- DAT/Assets/Script/UI/MouseCursorFollower.cs
+++ DAT/Assets/Script/UI/MouseCursorFollower.cs
@@ -1,20 +1,19 @@
 using UnityEngine;
 
-public class SimpleCursor : MonoBehaviour
+public class CursorUI : MonoBehaviour
 {
     void Start()
     {
         // ゲーム開始時に、ヒエラルキー内で自分を一番下に移動させる
         transform.SetAsLastSibling();
+
+       
     }
 
     void Update()
     {
         transform.position = Input.mousePosition;
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Cursor.visible = false;
-        }
+        Cursor.visible = false;
     }
 }

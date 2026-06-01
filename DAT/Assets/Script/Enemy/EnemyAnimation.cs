1d1069a5f48bc8e00cb96d80306da14a65f5c9ce
hira774 <fko2647012@stu.o-hara.ac.jp>
2026-06-01 18:22:04 +0900

    攻撃

--- DAT/Assets/Script/Enemy/EnemyAnimation.cs
+++ DAT/Assets/Script/Enemy/EnemyAnimation.cs
@@ -5,13 +5,13 @@ using UnityEngine.InputSystem.LowLevel;
 // 列挙型 enum
 // アニメーションの状態
 // 待機、移動、攻撃 横、攻撃 上、攻撃 下
-public enum EnemyState { Idle, Walk, SideAttack, LowerAttack, UpperAttack, Init }
+public enum EnemyAnimState { Idle, Walk, SideAttack, LowerAttack, UpperAttack, Init }
 
 // 構造体 struct
 [System.Serializable]
 public struct AnimationData
 {
-    public EnemyState state;            // 状態 (待機/移動/攻撃)
+    public EnemyAnimState state;            // 状態 (待機/移動/攻撃)
     public Sprite[] sprites;            // 使用するスプライトの配列
     public int frameRate;               // アニメーションの再生速度 (fps)
     public bool isLoop;                 // ループ再生するかどうか
@@ -21,16 +21,17 @@ public class EnemyAnimation : MonoBehaviour
 {
     [SerializeField] private List<AnimationData> animationDatas = new List<AnimationData>();
     // 辞書機能
-    private Dictionary<EnemyState, AnimationData> animDictionary = new Dictionary<EnemyState, AnimationData>();
+    private Dictionary<EnemyAnimState, AnimationData> animDictionary = new Dictionary<EnemyAnimState, AnimationData>();
 
     // current = 現在
     private AnimationData animData;
     private AnimationData currentActiveData;
-    [SerializeField] private EnemyState currentState = EnemyState.Init;
+    [SerializeField] private EnemyAnimState currentState = EnemyAnimState.Init;
     [SerializeField] private int currentFrame = 0;   // 現在のフレーム
     [SerializeField] private float timer = 0;
     [SerializeField] private float timePerFrame = 0;
 
+    EnemyController enemyCtrl;
     private SpriteRenderer spr;
 
     // 先に済ませないとtimePerFrameがInfinityになる(0で除算してしまう)
@@ -50,11 +51,11 @@ public class EnemyAnimation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        enemyCtrl = GetComponent<EnemyController>();
 
         spr = GetComponent<SpriteRenderer>();
 
-        ChangeState(EnemyState.Idle);
+        ChangeState(EnemyAnimState.Idle);
 
         //timePerFrame = 1f / animData.frameRate; // 1フレームにかかる時間を計算
     }
@@ -80,7 +81,9 @@ public class EnemyAnimation : MonoBehaviour
                 }
                 else
                 {
-                    ChangeState(EnemyState.Idle);
+                    enemyCtrl.FinishAnim();
+                    enemyCtrl.IsAttackFalse();
+                    ChangeState(EnemyAnimState.Idle);
                 }
             }
             else
@@ -92,7 +95,7 @@ public class EnemyAnimation : MonoBehaviour
         }
     }
 
-    public void ChangeState(EnemyState changedState)
+    public void ChangeState(EnemyAnimState changedState)
     {
         if (currentState == changedState) return;
         currentState = changedState;
@@ -104,7 +107,7 @@ public class EnemyAnimation : MonoBehaviour
             currentFrame = 0;
             timer = 0f;
 
-            Debug.Log("statusを変更しました：");
+            //Debug.Log("statusを変更しました：");
 
             // 新しいスプライトのフレームレートに変更
             timePerFrame = 1f / currentActiveData.frameRate;

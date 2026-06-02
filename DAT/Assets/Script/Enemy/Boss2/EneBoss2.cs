5ba920be91284b0219daee431aa18445f4adc4bb
asohiroto <fko2647059@stu.o-hara.ac.jp>
2026-06-02 18:50:56 +0900

    ボス敵が指定の場所まで動く処理

--- DAT/Assets/Script/Enemy/Boss2/EneBoss2.cs
+++ DAT/Assets/Script/Enemy/Boss2/EneBoss2.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EneBoss2 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    float speed = 0.1f;
+    Vector3 moveDir;
+    Vector3 currentPos;
     void Start()
     {
-        
+        currentPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move(new Vector3(5, 0, 0));
+        transform.position = currentPos;
+    }
+
+    // 指定した座標に移動する
+    void Move(Vector3 position)
+    {
+        if ((currentPos.x - position.x < 0.5f&& currentPos.x - position.x > -0.5f )
+            && currentPos.y - currentPos.y < 0.5f && currentPos.y - currentPos.y > -0.5f)
+        {
+            return;
+        }
+        moveDir = position - currentPos;
+        moveDir = moveDir.normalized;
+        currentPos += moveDir * speed;
     }
 }

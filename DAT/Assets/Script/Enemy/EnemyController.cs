c75968735e6a83218d4ce029ec625294298bc81e
hira774 <fko2647012@stu.o-hara.ac.jp>
2026-06-04 00:41:08 +0900

    新しい行動処理　途中です

--- DAT/Assets/Script/Enemy/EnemyController.cs
+++ DAT/Assets/Script/Enemy/EnemyController.cs
@@ -232,7 +232,7 @@ public class EnemyController : MonoBehaviour
         
     }
 
-    public void IsAttackFalse()
+    public void AttackFin()
     {
         isAttack = false;
         isAttackCool = false;

f74a7c6d8a034a6306b72ec74aef8f33547df6d0
FukudaGenki <fko2647053@stu.o-hara.ac.jp>
2026-06-04 01:11:27 +0900

    ボス敵の範囲攻撃（仮）

--- DAT/Assets/Script/Enemy/Boss2/EneBoss2.cs
+++ DAT/Assets/Script/Enemy/Boss2/EneBoss2.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UIElements;
 
 public class EneBoss2 : MonoBehaviour
@@ -6,6 +8,11 @@ public class EneBoss2 : MonoBehaviour
     float speed = 0.1f;
     Vector3 moveDir;
     Vector3 currentPos;
+    bool endMove = false;
+    // 範囲攻撃の変数
+    int startRangeAttackFrame = 5;
+    int endRangeAttackFrame = 5;
+    [SerializeField]GameObject rangeAttackObj;
     void Start()
     {
         currentPos = transform.position;
@@ -18,15 +25,24 @@ public class EneBoss2 : MonoBehaviour
     }
 
     // 指定した座標に移動する
-    void Move(Vector3 position)
+    void Move(Vector3 targetPos)
     {
-        if ((currentPos.x - position.x < 0.5f&& currentPos.x - position.x > -0.5f )
+        if (currentPos.x - targetPos.x < 0.5f&& currentPos.x - targetPos.x > -0.5f
             && currentPos.y - currentPos.y < 0.5f && currentPos.y - currentPos.y > -0.5f)
         {
+            if(endMove) return;
+            Debug.Log("移動完了！");
+            Stanp();
+            endMove = true;
             return;
         }
-        moveDir = position - currentPos;
+        moveDir = targetPos - currentPos;
         moveDir = moveDir.normalized;
         currentPos += moveDir * speed;
+    }
+
+    void Stanp()
+    {
+        Instantiate(rangeAttackObj, this.transform);
     }
 }

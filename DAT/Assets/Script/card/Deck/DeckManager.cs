5296c464e3869fddd629e30747fea8de007fca34
asohiroto <fko2647059@stu.o-hara.ac.jp>
2026-06-05 19:06:05 +0900

    iroiro

---
+++ DAT/Assets/Script/card/Deck/DeckManager.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    [SerializeField] public GameObject[] deckPrefab;
+    [SerializeField] public GameObject[] cardPrefab;
+
+    [SerializeField] public Transform[] deckTrans;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

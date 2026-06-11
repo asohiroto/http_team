06f7ddee9ee7c0c748ae977e7b155ebe002225d7
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-06-11 21:58:27 +0900

    タイトルから操作方法シーン、リザルトシーンからタイトルシーンに行くときにBGMが消えないように設定

---
+++ DAT/Assets/Script/Scene/TitleBgm.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleBgm : MonoBehaviour
+{
+    private static TitleBgm instance;
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "StartScene")
+        {
+            // タイトルに戻ったら再生
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else if (scene.name == "AllSceneProto" || scene.name == "ClearScene" || scene.name == "GameOverScene")
+        {
+            // クリアやゲームオーバーでは停止
+            audioSource.Stop();
+        }
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}

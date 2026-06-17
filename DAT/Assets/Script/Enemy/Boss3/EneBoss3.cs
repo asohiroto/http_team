cabf5b173912e5cacc433cba47abc421dce45829
FukudaGenki <fko2647053@stu.o-hara.ac.jp>
2026-06-17 16:52:06 +0900

    aset追加

---
+++ DAT/Assets/Script/Enemy/Boss3/EneBoss3.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EneBoss3 : MonoBehaviour
+{
+    enum State { Idle, Shot, Attack2, Attack3 };
+    State state = 0;
+    // Shotに使う変数---------------------------
+    enum ShotState { Move, GeneShot};
+    ShotState shotState = 0;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shot()
+    {
+
+    }
+}

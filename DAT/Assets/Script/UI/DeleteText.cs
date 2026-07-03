3352d473ede298ceeaf419188f4803cc513b71b4
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-07-03 11:10:57 +0900

    合成ボタン消したりいろいろ

---
+++ DAT/Assets/Script/UI/DeleteText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DeleteText : MonoBehaviour
+{
+    public GameObject textObject;
+
+    public void HideText()
+    { 
+        if(textObject != null)
+        {
+            // テキスト非表示
+            textObject.SetActive(false);
+        }
+
+    }
+}
---
+++ DAT/Assets/Script/UI/UiFront.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class UiFront : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // ゲーム開始時に、ヒエラルキー内で自分を一番下に移動させる
+        transform.SetAsLastSibling();
+    }
+}

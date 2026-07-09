fb7824be1990cbf8fa8e23d9b6c0a16edba97556
TeraoAyato <fko2647029@stu.o-hara.ac.jp>
2026-07-09 13:24:29 +0900

    Boss2のHpUi更新

---
+++ DAT/Assets/Script/UI/BossHpUi.cs
@@ -0,0 +1,31 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHpUi : MonoBehaviour
+{
+    [Header("対象ボス")]
+    [SerializeField] private EnemyHpManager bossHpManager;
+
+    [Header("参照UI")]
+    [SerializeField] private Image hpImage; // HPバー
+
+    private float maxHp;
+    void Start()
+    {
+        if (bossHpManager != null)
+        {
+            maxHp = bossHpManager.GetCurrentHp();
+        }
+    }
+
+    
+    void Update()
+    {
+        if (bossHpManager == null) return;
+
+        float currentHp = bossHpManager.GetCurrentHp();
+
+        hpImage.fillAmount = currentHp / maxHp;
+    }
+}
